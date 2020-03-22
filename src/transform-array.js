bcb5ed9699fed1c6d83a05e94f9067afcf1dd6c0
TatarskiyPavel <Pasha.Tatarsky@gmal.com>
2020-03-22 18:10:18 +0200

    init: Transform array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,3 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+
 };
