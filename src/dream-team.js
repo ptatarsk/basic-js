63d985f67584b1c12bf3fd256128b37b6e9bbcd9
TatarskiyPavel <Pasha.Tatarsky@gmal.com>
2020-03-22 16:26:51 +0200

    feat: done Dream team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,3 +1,11 @@
 module.exports = function createDreamTeam(members) {
+  let team = [];
 
+  if (!Array.isArray(members))
+    return false;
+  members.forEach((el) => {
+    if (typeof el === 'string')
+      team.push(el.trim()[0].toUpperCase());
+  });
+  return team.sort().join('');
 };
