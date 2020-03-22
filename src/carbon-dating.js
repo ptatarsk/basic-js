893e72fa81c5e07cb9b7586ff4df848deffcfebb
TatarskiyPavel <Pasha.Tatarsky@gmal.com>
2020-03-22 16:04:31 +0200

    feat: done Carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,5 +2,11 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  
+  const sample_activity = parseFloat(sampleActivity);
+  const rate_constant = 0.693 / HALF_LIFE_PERIOD;
+
+  if (!sample_activity || (typeof sampleActivity !== 'string') ||
+  (sample_activity <= 0) || sample_activity > MODERN_ACTIVITY)
+    return false;
+  return Math.ceil(Math.log(MODERN_ACTIVITY / sample_activity) / rate_constant);
 };
