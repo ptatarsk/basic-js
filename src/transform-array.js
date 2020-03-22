59413d9730a0fa76757e21a73e912304612d33a9
TatarskiyPavel <Pasha.Tatarsky@gmal.com>
2020-03-22 19:08:43 +0200

    feat: done Transform array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,3 +1,27 @@
 module.exports = function transform(arr) {
+    let res = [];
+    const operations = ['--double-prev', '--double-next',
+    '--discard-prev', '--discard-next'];
 
+    if (!Array.isArray(arr))
+        throw new Error();
+    for (let i = 0; i < arr.length; i++) {
+        if (operations.includes(arr[i])) {
+            if (arr[i] == operations[0]) {
+                if (i > 0)
+                    res.push(arr[i - 1]);
+            } else if (arr[i] == operations[1]) {
+                if (i < arr.length - 1)
+                    res.push(arr[i + 1]);
+            } else if (arr[i] == operations[2]) {
+                if (res.length)
+                    res.pop();
+            } else {
+                i++;
+            }
+        } else {
+            res.push(arr[i]);
+        }
+    }
+    return res;
 };
