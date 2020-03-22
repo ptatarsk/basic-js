0543812c8afd3d27a6b3d2ab3accc9ee7c07b794
TatarskiyPavel <Pasha.Tatarsky@gmal.com>
2020-03-22 17:40:55 +0200

    feat: done What season? task

--- src/what-season.js
+++ src/what-season.js
@@ -1,3 +1,10 @@
 module.exports = function getSeason(date) {
-  
+  const seasons = ['winter', 'winter', 'spring', 'spring', 'spring',
+  'summer', 'summer', 'summer', 'autumn', 'autumn', 'autumn', 'winter'];
+
+  if (!date)
+    return 'Unable to determine the time of year!';
+  if (!date.getTime())
+    throw new Error();
+  return seasons[date.getMonth()];
 };
