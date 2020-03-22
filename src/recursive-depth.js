4456cb36819f1feb3c144808f4bc4a94725edaa1
TatarskiyPavel <Pasha.Tatarsky@gmal.com>
2020-03-22 20:28:22 +0200

    init: Recursive depth calculator task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,5 @@
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    calculateDepth(arr) {
+
     }
 };
