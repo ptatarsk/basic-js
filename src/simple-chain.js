72c1f72b0ea3eef9645104d6cd3677685793dd71
TatarskiyPavel <Pasha.Tatarsky@gmal.com>
2020-03-22 20:21:33 +0200

    feat: done Chain maker task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,18 +1,38 @@
 const chainMaker = {
+  chain: [],
+
   getLength() {
-
+    return this.chain.length;
   },
+
   addLink(value) {
-
+    this.chain.push(value);
+    return this;
   },
+
   removeLink(position) {
-
+    if (isNaN(position) || position < 1 || position >= this.chain.length) {
+      this.chain = [];
+      throw new Error();
+    } else {
+      this.chain.splice(position - 1, 1);
+      return this;
+    }
   },
+
   reverseChain() {
-
+    this.chain = this.chain.reverse();
+    return this;
   },
-  finishChain() {
 
+  finishChain() {
+    let res = [];
+
+    this.chain.map((el) => {
+      res.push(`( ${el} )`);
+    });
+    this.chain = [];
+    return res.join('~~');
   }
 };
 
