9cbeae251f51fd833a3eb0eda2b8686f9e9e9edd
TatarskiyPavel <Pasha.Tatarsky@gmal.com>
2020-03-22 18:08:52 +0200

    feat: done Tower of Hanoi task

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,3 +1,6 @@
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+    let turns_calc = Math.pow(2, disksNumber) - 1;
+    let seconds_calc = turns_calc / (turnsSpeed / 3600);
 
+    return {turns: turns_calc, seconds: seconds_calc};
 }
