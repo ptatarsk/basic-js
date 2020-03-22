302722f6c6e170dffabbb140fe0315bf1c815180
TatarskiyPavel <Pasha.Tatarsky@gmal.com>
2020-03-22 22:32:40 +0200

    feat: done Recursive depth calculator task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,5 +1,14 @@
 module.exports = class DepthCalculator {
     calculateDepth(arr) {
-
+        let depth = 0;
+        arr.forEach((el) => {
+                if (Array.isArray(el)) {
+                    let cur_depth = this.calculateDepth(el);
+                    if (cur_depth > depth)
+                        depth = cur_depth;
+                }
+            }
+        );
+        return ++depth;
     }
-};
+}
